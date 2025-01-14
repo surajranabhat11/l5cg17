144eed19895aee9ed339df154bc2741d202b5687
Suaj Ranabhat <np03cs4a230400@heraldcollege.edu.np>
2025-01-14 15:34:51 +0545

    Added student.java

---
+++ student.java
@@ -0,0 +1,20 @@
+public class student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+}
